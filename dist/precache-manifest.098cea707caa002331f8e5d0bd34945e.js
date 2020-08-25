a131d0168f2b89a425659148727978b5e45f27ed
virtu78 <virtu78@yandex.ru>
2020-08-25 15:44:46 +0500

    Initial dist subtree commit

---
+++ dist/precache-manifest.098cea707caa002331f8e5d0bd34945e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20aaf3f5b5410683625a",
+    "url": "/phisikon/css/app.278124db.css"
+  },
+  {
+    "revision": "db10420c42091aa3ae6e0048b08fe133",
+    "url": "/phisikon/img/globus.db10420c.png"
+  },
+  {
+    "revision": "0831ddf4f96b1517ab54b0bd742b2a11",
+    "url": "/phisikon/img/preloader.0831ddf4.gif"
+  },
+  {
+    "revision": "11ece4f861eb1d05ab9bcc5f060c5a23",
+    "url": "/phisikon/index.html"
+  },
+  {
+    "revision": "20aaf3f5b5410683625a",
+    "url": "/phisikon/js/app.00ceef34.js"
+  },
+  {
+    "revision": "5357e95eb8eb65091abd",
+    "url": "/phisikon/js/chunk-vendors.0c88f085.js"
+  },
+  {
+    "revision": "79d4a12bf6fe3d22f09ed8a2bb3be427",
+    "url": "/phisikon/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/phisikon/robots.txt"
+  }
+]);
